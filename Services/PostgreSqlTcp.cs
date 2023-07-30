6529aefbba1355f3ba005cea82dc9bf118ea84c5
DellLin <avercker@gmail.com>
2023-07-30 21:31:48 +0800

    update

--- Services/PostgreSqlTcp.cs
+++ Services/PostgreSqlTcp.cs
@@ -4,6 +4,10 @@ public class PostgreSqlTcp
     {
         public static NpgsqlConnectionStringBuilder NewPostgreSqlTCPConnectionString()
         {
+            System.Console.WriteLine(Environment.GetEnvironmentVariable("INSTANCE_CONNECTION_NAME"));
+            System.Console.WriteLine(Environment.GetEnvironmentVariable("DB_USER"));
+            System.Console.WriteLine(Environment.GetEnvironmentVariable("DB_PASS"));
+            System.Console.WriteLine(Environment.GetEnvironmentVariable("DB_NAME"));
             // Equivalent connection string:
             // "Uid=<DB_USER>;Pwd=<DB_PASS>;Host=<INSTANCE_HOST>;Database=<DB_NAME>;"
             var connectionString = new NpgsqlConnectionStringBuilder()
@@ -12,7 +16,8 @@ public class PostgreSqlTcp
                 // secure - consider a more secure solution such as
                 // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
                 // keep secrets safe.
-                Host = Environment.GetEnvironmentVariable("INSTANCE_CONNECTION_NAME"),     // e.g. '127.0.0.1'
+                // Host = Environment.GetEnvironmentVariable("INSTANCE_CONNECTION_NAME"),     // e.g. '127.0.0.1'
+                Host = "34.80.75.228",
                 // Set Host to 'cloudsql' when deploying to App Engine Flexible environment
                 Username = Environment.GetEnvironmentVariable("DB_USER"), // e.g. 'my-db-user'
                 Password = Environment.GetEnvironmentVariable("DB_PASS"), // e.g. 'my-db-password'
@@ -22,9 +27,19 @@ public class PostgreSqlTcp
                 // SslPassword = Environment.GetEnvironmentVariable("DB_CLIENT_KEY"),
                 // The Cloud SQL proxy provides encryption between the proxy and instance.
                 // TrustServerCertificate = false,
-                // SslMode = SslMode.VerifyCA,
+                SslMode = SslMode.Disable,
                 
             };
+             // [END cloud_sql_postgres_dotnet_ado_connect_tcp]
+            // For deployments that connect directly to a Cloud SQL instance without
+            // using the Cloud SQL Proxy, configuring SSL certificates will ensure the
+            // connection is encrypted.
+            // if (Environment.GetEnvironmentVariable("DB_CERT") != null)
+            // {
+            //     connectionString.SslMode = SslMode.VerifyCA;
+            //     connectionString.SslCertificate =
+            //         Environment.GetEnvironmentVariable("/Users/delllin/Projects/GCP/Cert/client.pfx"); // e.g. 'certs/client.pfx'
+            // }
             connectionString.Pooling = true;
             // Specify additional properties here.
             return connectionString;
